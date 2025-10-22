de2fd52136eb4d80bc0fa70e6b1a5caaf41c2f9e
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-10-22 17:09:26 +0300

    Add files via upload

---
+++ ClutchApi/src/main/java/com/example/ClutchApi/ClutchApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClutchApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClutchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClutchApiApplication.class, args);
+	}
+
+}
---
+++ ClutchApi/src/test/java/com/example/ClutchApi/ClutchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClutchApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClutchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
